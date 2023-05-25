c05a7cc93adac352381c254dfdff2f0edb3de007
seungwoo <80995345+iamseungwoo@users.noreply.github.com>
2023-05-25 03:03:13 +0000

    https사이트에서 http 사이트 접속시 에러 해결

--- class/pages/_app.js
+++ class/pages/_app.js
@@ -1,3 +1,21 @@
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+    // graphql setting
+    const client = new ApolloClient({
+        uri: "http://backend-example.codebootcamp.co.kr/graphql",
+        cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장해 놓기 => 나중에 더 자세히 알아보기
+    });
+
+    return (
+        <ApolloProvider client={client}>
+            <Head>
+                <meta
+                    httpEquiv="Content-Security-Policy"
+                    content="upgrade-insecure-requests"
+                />
+            </Head>
+            <Component {...pageProps} />
+        </ApolloProvider>
+    );
 }
